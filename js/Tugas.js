b4228e20e8bd5434fa9d2784b3f4e5716c55612d
ShofiFarihahMuazarah <91237059+ShofiFarihahMuazarah@users.noreply.github.com>
2021-12-21 15:45:35 +0700

    Add files via upload

---
+++ js/Tugas.js
@@ -0,0 +1,26 @@
+document.getElementById("Jumlah").onclick = function() {TampilkanNilai()};
+function TampilkanNilai() {
+  var NilaiDadu1 = document.getElementById("Nilai1").innerHTML;
+  var NilaiDadu2 = document.getElementById("Nilai2").innerHTML;
+  total = Number(NilaiDadu1)+Number(NilaiDadu2);
+  return alert(`Anda Maju Sebanyak ${total} Langkah`)
+}
+
+document.getElementById("bt1").onclick = function() {Acak1()};
+function Acak1() {
+  var Dadu1 = Math.floor(Math.random() * (7 - 1) ) + 1;
+  document.getElementById("Nilai1").innerHTML = Dadu1;
+  document.getElementById("Dadu1").src = "dice"+Dadu1+".png"
+}
+
+document.getElementById("bt2").onclick = function() {Acak2()};
+function Acak2() {
+  var Dadu2 = Math.floor(Math.random() * (7 - 1) ) + 1;
+  document.getElementById("Nilai2").innerHTML = Dadu2;
+  document.getElementById("Dadu2").src = "dice"+Dadu2+".png"
+}
+document.getElementById("RANDOM").onclick = function() {RANDOM()};
+  function RANDOM() {
+    Acak1();
+    Acak2();
+}
